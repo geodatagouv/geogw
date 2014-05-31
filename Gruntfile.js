00ff2d1e6f57321a5064704167845358ab001e58
Jérôme Desboeufs <jerome.desboeufs@gmail.com>
2014-05-31 22:19:07 +0200

    Enforce JSHint check

--- Gruntfile.js
+++ Gruntfile.js
@@ -4,19 +4,45 @@ module.exports = function(grunt) {
     grunt.initConfig({
 
         jshint: {
-            all: [
-                '*.js',
-                'server/**/*.js',
-                'app/js/**/*.js'
-            ],
+            server: {
+                files: {
+                    src: ['*.js', 'server/**/*.js']
+                },
+                options: {
+                    node: true
+                }
+            },
+            browser: {
+                files: {
+                    src: ['app/**/*.js']
+                },
+                options: {
+                    browser: true,
+                    devel: true,
+                    globals: {
+                        angular: true
+                    }
+                }
+            },
             options: {
-
+                bitwise: true,
+                eqeqeq: true,
+                freeze: true,
+                immed: true,
+                indent: 4,
+                latedef: true,
+                newcap: true,
+                nonew: true,
+                quotmark: 'single',
+                undef: true,
+                unused: true,
+                trailing: true,
             }
         }
 
     });
 
     grunt.registerTask('default', [
-        'jshint:all'
+        'jshint'
     ]);
 };
