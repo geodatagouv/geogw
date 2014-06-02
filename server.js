6049d57a502f526b293b4700138c4716a132d8e2
Jérôme Desboeufs <jerome.desboeufs@gmail.com>
2014-06-02 11:34:15 +0200

    Update production log definition

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ var app = express();
 
 if (process.env.NODE_ENV === 'production') {
     app.set('trust proxy', true);
-    app.use(morgan());
+    app.use(morgan(':req[x-real-ip] - - [:date] ":method :url HTTP/:http-version" :status - :response-time ms - :res[content-length] ":referrer"'));
 } else {
     app.use(morgan('dev'));
 }
