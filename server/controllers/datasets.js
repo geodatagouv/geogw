fa528551061f83bd69c490ea20ec19ef84f12b8d
Jérôme Desboeufs <jerome.desboeufs@gmail.com>
2014-06-13 18:05:18 +0200

    Add pagination to search

--- server/controllers/datasets.js
+++ server/controllers/datasets.js
@@ -4,18 +4,21 @@
 var mongoose = require('mongoose');
 var Record = mongoose.model('Record');
 var async = require('async');
+var _ = require('lodash');
 
 exports.search = function(req, res, next) {
+    var q, limit, offset, opendata;
+    if (req.query.q && _.isString(req.query.q) && req.query.q.length) q = req.query.q;
+    limit = parseInt(req.query.limit);
+    limit = _.isNumber(limit) && limit > 0 && limit <= 100 ? Math.floor(limit) : 20;
+    offset = parseInt(req.query.offset);
+    offset = _.isNumber(offset) && offset > 0 ? Math.floor(offset) : 0;
+    opendata = req.query.opendata === 'true';
+
     function buildQuery() {
         var query = Record.find().where('metadata.type').in(['dataset', 'series']);
-        if (req.query.q && req.query.q.length) {
-            query.where({ $text: { $search: req.query.q, $language: 'french' }});
-        }
-        if (req.query.opendata === 'true') {
-            query
-                .where('metadata.keywords')
-                .in(['donnée ouverte', 'données ouvertes', 'opendata']);
-        }
+        if (q) query.where({ $text: { $search: q, $language: 'french' }});
+        if (opendata) query.where('metadata.keywords').in(['donnée ouverte', 'données ouvertes', 'opendata']);
         if (req.service) query.where('parentCatalog', req.service.id);
         return query;
     }
@@ -25,7 +28,8 @@ exports.search = function(req, res, next) {
             buildQuery()
                 .select({ score: { $meta: 'textScore' } })
                 .sort({ score: { $meta: 'textScore' } })
-                .limit(20)
+                .skip(offset)
+                .limit(limit)
                 .exec(callback);
         },
         count: function(callback) {
@@ -33,6 +37,8 @@ exports.search = function(req, res, next) {
         }
     }, function(err, out) {
         if (err) return next(err);
+        out.limit = limit;
+        out.offset = offset;
         res.json(out);
     });
 };
