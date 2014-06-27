39152b4eb9e195a6a139f29574e71fb8a028dddb
Jérôme Desboeufs <jerome.desboeufs@gmail.com>
2014-06-27 17:55:19 +0200

    Add more root certs

--- worker.js
+++ worker.js
@@ -1,6 +1,8 @@
 var jobs = require('./server/kue').jobs;
 var cswHarvester = require('./server/workers/csw');
 
+require('ssl-root-cas/latest').inject();
+
 jobs.process('harvest', 2, function(job, done) {
     cswHarvester.harvest(job, done);
 });
