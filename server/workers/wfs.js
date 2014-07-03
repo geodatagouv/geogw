3ebeb0d30e8278a5397a37ecaab5be62bdc7a72a
Jérôme Desboeufs <jerome.desboeufs@gmail.com>
2014-07-03 10:22:19 +0200

    Add WFS service sync

---
+++ server/workers/wfs.js
@@ -0,0 +1,30 @@
+var wfs = require('wfs-client');
+var mongoose = require('../mongoose');
+var ServiceSync = mongoose.model('ServiceSync');
+
+function lookupService(serviceSync, job, done) {
+    var client = wfs(serviceSync.service.location, { 
+        userAgent: 'Afigeo WFS harvester',
+        queryStringToAppend: serviceSync.service.locationOptions.query
+    });
+
+    client.capabilities().then(function(capabilities) {
+        console.log(capabilities.featureTypes);
+        serviceSync.toggleSuccessful(capabilities.featureTypes.length, done);
+    }, function(e) {
+        serviceSync.toggleError(function(err) {
+            if (err) {
+                console.trace(err);
+                job.log('Unable to persist status `failed`. Error has been traced to console');
+            }
+            done(e);
+        });
+    });
+}
+
+exports.lookup = function(job, done) {
+    ServiceSync.findByIdAndProcess(job.data.serviceSyncId, job.id, function(err, serviceSync) {
+        if (err) return done(err);
+        lookupService(serviceSync, job, done);
+    });
+};
--- worker.js
+++ worker.js
@@ -1,10 +1,12 @@
 var jobs = require('./server/kue').jobs;
 var csw = require('./server/workers/csw');
+var wfs = require('./server/workers/wfs');
 
 require('ssl-root-cas/latest').inject();
 
 jobs.process('service-sync', 2, function(job, done) {
     if (job.data.protocol === 'csw') csw.harvest(job, done);
+    else if (job.data.protocol === 'wfs') wfs.lookup(job, done);
     else done(new Error('Unknown protocol'));
 });
 
