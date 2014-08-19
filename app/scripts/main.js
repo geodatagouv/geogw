f3efa49be03a4098579e37e68e072c0a46ec4373
Jérôme Desboeufs <jerome.desboeufs@gmail.com>
2014-08-19 16:47:55 +0200

    Rename mainApp instance

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,14 +1,14 @@
 // Setup moment locale
 require('moment/lang/fr');
 
-var mainApp = angular.module('mainApp', ['ngRoute']);
+var app = angular.module('mainApp', ['ngRoute']);
 
-mainApp.filter('duration', require('./filters/duration'));
-mainApp.filter('timeago', require('./filters/timeago'));
-mainApp.filter('prune', require('./filters/prune'));
+app.filter('duration', require('./filters/duration'));
+app.filter('timeago', require('./filters/timeago'));
+app.filter('prune', require('./filters/prune'));
 
-mainApp.config(require('./routes'));
+app.config(require('./routes'));
 
-mainApp.controller('ServicesCtrl', require('./controllers/services'));
-mainApp.controller('ServiceDatasetsCtrl', require('./controllers/serviceDatasets'));
-mainApp.controller('DatasetCtrl', require('./controllers/dataset'));
+app.controller('ServicesCtrl', require('./controllers/services'));
+app.controller('ServiceDatasetsCtrl', require('./controllers/serviceDatasets'));
+app.controller('DatasetCtrl', require('./controllers/dataset'));
