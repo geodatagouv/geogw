a74c96cf5dac7678b5379e5268f60001aaacfc14
Jérôme Desboeufs <jerome.desboeufs@gmail.com>
2014-09-27 23:29:14 +0200

    Rewrite kue redis connection

--- server/kue.js
+++ server/kue.js
@@ -4,5 +4,13 @@
 var kue = require('kue');
 var redis = require('./redis');
 
-exports.jobs = kue.createQueue({ disableSearch: true });
+exports.jobs = kue.createQueue({
+    disableSearch: true,
+    redis: {
+        createClientFactory: function() {
+            return redis.createClient();
+        }
+    }
+});
+
 exports.Job = kue.Job;
