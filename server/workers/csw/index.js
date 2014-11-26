603360f587d074d4df6af2bad4c9ae1225f2e9fa
Jérôme Desboeufs <jerome.desboeufs@gmail.com>
2014-11-26 16:03:37 +0100

    Partially rewrite CSW harvester

--- server/workers/csw/index.js
+++ server/workers/csw/index.js
@@ -1,5 +1,6 @@
 var _ = require('lodash');
 var csw = require('csw-client');
+var iso19139 = require('iso19139');
 var mongoose = require('../../mongoose');
 var Record = mongoose.model('Record');
 var ServiceSync = mongoose.model('ServiceSync');
@@ -41,51 +42,59 @@ var createNewRecord = function(job, data, serviceSync, done) {
     updateRecord(job, record, data, serviceSync, done);
 };
 
-var processRecord = function(job, data, serviceSync, done) {
-    var name = data.title || data.name || data.fileIdentifier;
+var processRecord = function(job, rawRecord, serviceSync, done) {
+    if (!rawRecord.fileIdentifier) {
+        job.log('Dropping 1 record: no fileIdentifier set!');
+        return done();
+    }
+
+    var name = rawRecord.title || rawRecord.name || rawRecord.fileIdentifier;
     // debug('processing record %s', name);
 
     // Convert input date
-    if (data._updated) data._updated = moment(data._updated).toDate();
+    if (rawRecord._updated) rawRecord._updated = moment(rawRecord._updated).toDate();
 
     Record
-        .findOne({ identifier: data.fileIdentifier, parentCatalog: serviceSync.service._id })
+        .findOne({ identifier: rawRecord.fileIdentifier, parentCatalog: serviceSync.service._id })
         .exec(function(err, record) {
             if (err) return done(err);
 
             if (!record) {
                 job.log('New record ' + name);
-                createNewRecord(job, data, serviceSync, done);
+                createNewRecord(job, rawRecord, serviceSync, done);
             } else {
-                if (!data._updated && record.metadata._updated) {
+                if (!rawRecord._updated && record.metadata._updated) {
                     job.log('Dropping 1 record: no more _updated attribute!');
                     done();
-                } else if (data._updated && moment(record.metadata._updated).isSame(data._updated)) {
+                } else if (rawRecord._updated && moment(record.metadata._updated).isSame(rawRecord._updated)) {
                     job.log('Record ' + name + ': not updated');
                     // record.set('lastSync', serviceSync._id).save(done);
-                    updateRecord(job, record, data, serviceSync, done);
-                } else if (data._updated && moment(record.metadata._updated).isAfter(data._updated)) {
+                    updateRecord(job, record, rawRecord, serviceSync, done);
+                } else if (rawRecord._updated && moment(record.metadata._updated).isAfter(rawRecord._updated)) {
                     job.log('Record ' + name + ': older version found!!!');
                     done();
                 } else {
                     job.log('Record ' + name + ': new version found!');
-                    updateRecord(job, record, data, serviceSync, done);
+                    updateRecord(job, record, rawRecord, serviceSync, done);
                 }
             }
         });
 };
 
-var harvestService = function(serviceSync, job, done) {
+var harvestService = function(serviceSync, job, harvestDone) {
     var client = csw(serviceSync.service.location, {
         maxSockets: job.data.maxSockets || 5,
         keepAlive: true,
         retry: job.data.maxRetry || 3,
         userAgent: 'Afigeo CSW harvester',
-        queryStringToAppend: serviceSync.service.locationOptions.query
+        queryStringToAppend: serviceSync.service.locationOptions.query,
+        concurrency: 5
     });
 
     var harvesterOptions = {
-        mapper: 'iso19139',
+        typeNames: 'gmd:MD_Metadata',
+        outputSchema: 'http://www.isotc211.org/2005/gmd',
+        elementSetName: 'full',
         constraintLanguage: 'CQL_TEXT'
     };
 
@@ -94,55 +103,29 @@ var harvestService = function(serviceSync, job, done) {
 
     var harvester = client.harvest(harvesterOptions);
 
-    var total;
-
     harvester.on('error', function(err) {
         job.log(JSON.stringify(err));
         console.trace(err);
     });
 
-    harvester.on('start', function(stats) {
-        total = stats.matched;
-        job.log(JSON.stringify(stats));
+    harvester.on('started', function() {
+        job.log('Records matched: %d', this.matched);
     });
 
-    harvester.on('page', function(infos) {
-        if (infos.announced < infos.found) {
-            total -= (infos.announced - infos.found);
-            job.log('Notice: %d records found of %d announced!', infos.found, infos.announced);
-        }
-    });
-
-    harvester.on('end', function(err, stats) {
-        if (err) {
-            console.trace(err);
-            serviceSync.toggleError(function(mongoErr) {
-                if (mongoErr) {
-                    console.trace(mongoErr);
-                    job.log('Unable to persist status `failed`. Error has been traced to console');
-                }
-                done(err);
-            });
-        } else {
-            serviceSync.toggleSuccessful(stats.returned, done);
-        }
-    });
-
-    var q = async.queue(function (data, done) {
-        var record = data.record;
-        job.progress(data.stats.returned, total);
-
-        if (!record.fileIdentifier) {
-            job.log('Dropping 1 record: no fileIdentifier set!');
-            return done();
-        }
-
+    var q = async.queue(function (record, done) {
         processRecord(job, record, serviceSync, done);
     }, 1);
 
-    harvester.on('record', function(data) {
-        q.push(data);
-    });
+    harvester
+        .pipe(iso19139.parseAll())
+        .on('data', function(record) {
+            job.progress(harvester.returned, harvester.matched);
+            q.push(record);
+        })
+        .on('end', function() {
+            job.log('Records returned: %d', this.returned);
+            serviceSync.toggleSuccessful(this.returned, harvestDone);
+        });
 };
 
 exports.harvest = function(job, done) {
