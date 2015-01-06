2c4eef2a72ded0ae0b6c9a899e4ca5f4c1cfb426
Jérôme Desboeufs <jerome.desboeufs@gmail.com>
2015-01-06 12:27:25 +0100

    Fix code typo

--- app/scripts/controllers/navbar.js
+++
@@ -1,7 +0,0 @@
-module.exports = function($scope, SessionService) {
-    $scope.isLoggedIn = SessionService.isLoggedIn();
-
-    $scope.$on('userLoggedIn', function() {
-        $scope.isLoggedIn = true;
-    });
-};
