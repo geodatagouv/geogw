461ea0411a7c7432b9f1af750d2e5a83026e7368
Jérôme Desboeufs <jerome.desboeufs@gmail.com>
2015-01-06 19:51:43 +0100

    Add CORS headers

--- server/api.js
+++ server/api.js
@@ -1,10 +1,12 @@
 var express = require('express');
 var bodyParser = require('body-parser');
+var cors = require('cors');
 var passport = require('./passport');
 var utils = require('./utils');
 
 var app = express();
 
+app.use(cors());
 app.use(bodyParser.json());
 
 utils.walk(__dirname + '/routes', 'middlewares', function(path) {
