5fe2971f9a51b30179a9331330c4ad632bd88952
Jérôme Desboeufs <jerome.desboeufs@gmail.com>
2015-02-02 15:23:53 +0100

    Accept weak TLS servers

--- worker.js
+++ worker.js
@@ -3,6 +3,9 @@ var csw = require('./server/workers/harvest-csw');
 var wfs = require('./server/workers/lookup-wfs');
 var processRecord = require('./server/workers/process-record');
 
+// To remove in the future
+process.env.NODE_TLS_REJECT_UNAUTHORIZED = '0';
+
 require('ssl-root-cas/latest').inject();
 
 jobs.process('harvest-csw', 4, csw.harvest);
