9f0ac97e49a93f5af6207cb008e153c7bd3202da
Jérôme Desboeufs <jerome.desboeufs@gmail.com>
2015-04-27 19:06:49 +0200

    Add GA

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -18,7 +18,12 @@ app.controller('ServicesCtrl', require('./controllers/services'));
 app.controller('ServiceDatasetsCtrl', require('./controllers/serviceDatasets'));
 app.controller('DatasetCtrl', require('./controllers/dataset'));
 
-app.run(function(SessionService) {
+app.run(function($rootScope, $location, $window, SessionService) {
+    $rootScope.$on('$stateChangeSuccess', function (ev) {
+        if (!$window.ga) return;
+        $window.ga('send', 'pageview', { page: $location.path() });
+    });
+
     SessionService.init();
     SessionService.fetch();
 });
