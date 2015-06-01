996e59a8e010edca828d829682b52c0d277f923a
Jérôme Desboeufs <jerome.desboeufs@gmail.com>
2015-06-01 17:38:45 +0200

    Fix harvesting UI

--- app/scripts/controllers/services.js
+++ app/scripts/controllers/services.js
@@ -4,9 +4,11 @@ module.exports = function($scope, $http, $routeParams) {
     $scope.canBeSynced = function(service) {
         return service.syncEnabled && !service.sync.pending && !service.sync.processing;
     };
+    $scope.isNew = function (service) {
+        return !service.sync.status || service.sync.status === 'new';
+    };
     $scope.syncService = function(service) {
-        if (!service.lastSync) service.lastSync = {};
-        service.lastSync.status = 'queued';
+        service.sync.pending = true;
         $http.post('/api/services/' + service._id + '/sync');
     };
     $scope.fetchServices = function(protocol) {
