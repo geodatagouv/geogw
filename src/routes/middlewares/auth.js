1f874657f39aaa734e05b568367ee924a99b4d93
Jérôme Desboeufs <jerome.desboeufs@gmail.com>
2015-09-07 17:03:33 +0200

    Add maintenance ACL

--- src/routes/middlewares/auth.js
+++ src/routes/middlewares/auth.js
@@ -7,3 +7,10 @@ exports.isAdmin = function(req, res, next) {
     if (!req.user || !req.user.isAdmin) res.status(401).end();
     next();
 };
+
+exports.isMaintenance = function (req, res, next) {
+    if (req.user && req.user.isAdmin) return next();
+    if (req.query.token && process.env.MAINTENANCE_TOKEN && req.query.token === process.env.MAINTENANCE_TOKEN)
+        return next();
+    res.status(401).end();
+};
