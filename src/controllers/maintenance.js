fb86b984dd06160323df1e23e79a220edc332068
Jérôme Desboeufs <jerome.desboeufs@gmail.com>
2015-09-11 15:53:32 +0200

    Add consolidate-all-records maintenance task

--- src/controllers/maintenance.js
+++ src/controllers/maintenance.js
@@ -3,6 +3,7 @@ import through2 from 'through2';
 import mongoose from 'mongoose';
 
 const RecordRevision = mongoose.model('RecordRevision');
+const ConsolidatedRecord = mongoose.model('ConsolidatedRecord');
 
 
 export function processAllRecords(req, res) {
@@ -21,5 +22,24 @@ export function processAllRecords(req, res) {
         }))
         .on('end', () => {
             res.send({ task: 'process-all-records', status: 'ok', count });
+        });
+}
+
+export function consolidateAllRecords(req, res) {
+    let count = 0;
+    ConsolidatedRecord.find().select('recordId').lean().stream()
+        .pipe(through2.obj((record, enc, cb) => {
+            return sidekick('dataset:consolidate', { recordId: record.recordId })
+                .then(() => {
+                    count++;
+                    cb();
+                })
+                .catch(err => {
+                    console.error(err);
+                    cb(err);
+                });
+        }))
+        .on('end', () => {
+            res.send({ task: 'consolidate-all-records', status: 'ok', count });
         });
 }
--- src/routes/maintenance.js
+++ src/routes/maintenance.js
@@ -1,13 +1,14 @@
 import { Router } from 'express';
 
-import { processAllRecords } from '../controllers/maintenance';
+import { processAllRecords, consolidateAllRecords } from '../controllers/maintenance';
 import { isMaintenance } from './middlewares/auth';
 
 export default function (app) {
     const router = new Router();
 
     /* Routes */
-    router.get('/process-all-records', processAllRecords);
+    router.post('/process-all-records', processAllRecords);
+    router.post('/consolidate-all-records', consolidateAllRecords);
 
     app.use('/maintenance', isMaintenance, router);
 }
