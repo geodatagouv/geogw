f70e11eb17b6db75f4b6a37f6c2a65dc1c863ceb
Jérôme Desboeufs <jerome.desboeufs@gmail.com>
2016-12-02 13:42:42 +0100

    Disable synchronizeAll limit

--- plugins/publish-to-udata/jobs/synchronizeAll.js
+++ plugins/publish-to-udata/jobs/synchronizeAll.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 const Dataset = mongoose.model('Dataset');
 
 module.exports = function (job, jobDone) {
-  Dataset.find().limit(500).exec()
+  Dataset.find().limit().exec()
     .map(dataset => dataset.asyncUpdate(), { concurrency: 10 })
     .thenReturn()
     .then(jobDone)
