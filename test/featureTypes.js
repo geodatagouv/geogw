c6b65ea34c5a6784e4034f5cb5ddf18ad57a9e17
Jérôme Desboeufs <jerome.desboeufs@gmail.com>
2017-01-28 19:04:30 +0100

    Add normalizeFeatureTypeName helper

---
+++ test/featureTypes.js
@@ -0,0 +1,21 @@
+/* eslint-env mocha */
+const { normalizeFeatureTypeName } = require('../lib/helpers/featureType');
+const expect = require('expect.js');
+
+describe('normalizeFeatureTypeName()', () => {
+
+  describe('basic use cases', () => {
+    const testCases = {
+      'ABC:DEF:1234': 'def:1234',
+      'abcdef': 'abcdef',
+      'AZE_TYUP:TYUI_PP': 'tyui_pp',
+      'ABCDEF': 'abcdef'
+    };
+    Object.keys(testCases).forEach(testCase => {
+      it(`should normalize ${testCase} into ${testCases[testCase]}`, () => {
+        expect(normalizeFeatureTypeName(testCase)).to.be(testCases[testCase]);
+      });
+    });
+  });
+
+});
