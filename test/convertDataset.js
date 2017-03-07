ad099b9d5eae290da2ff0a26000fc7b172d5551e
Jérôme Desboeufs <jerome.desboeufs@gmail.com>
2017-03-07 21:12:41 +0100

    Add getAllContacts method

--- test/convertDataset.js
+++ test/convertDataset.js
@@ -1,5 +1,6 @@
 /* eslint-env mocha */
 const convertDataset = require('../lib/helpers/convertDataset');
+const { getAllContacts } = require('../lib/helpers/convertDataset/iso19139');
 const expect = require('expect.js');
 
 describe('convertDataset.getLicenseFromLinks()', () => {
@@ -23,4 +24,60 @@ describe('convertDataset.getLicenseFromLinks()', () => {
         });
     });
 
+});
+
+describe('convertDataset.getAllContacts()', () => {
+    describe('called with representative metadata', () => {
+      it('should return contacts array', () => {
+        const metadata = {
+          contact: {
+            organisationName: 'Tralala',
+            individualName: 'Régis',
+            role: 'OWNER',
+            contactInfo: {
+              address: {
+                deliveryPoint: '1 rue République',
+                postalCode: '75015',
+                city: 'Paris 15e',
+                electronicMailAddress: 'info@acme.org',
+                country: 'Groland',
+              },
+              phone: {
+                voice: '0147200001',
+                facSimile: '3615',
+              }
+            }
+          },
+          identificationInfo: {
+            pointOfContact: [
+              {
+                organisationName: 'ACME'
+              },
+              {
+                individualName: 'AAA'
+              }
+            ]
+          }
+        };
+        const result = getAllContacts(metadata);
+        expect(result).to.eql([
+          {
+            organizationName: 'Tralala',
+            role: 'owner',
+            address: '1 rue République',
+            postalCode: '75015',
+            town: 'Paris 15e',
+            email: 'info@acme.org',
+            country: 'Groland',
+            phoneNumber: '0147200001',
+            relatedTo: 'metadata',
+          },
+          {
+            organizationName: 'ACME',
+            role: 'notDefined',
+            relatedTo: 'data',
+          }
+        ]);
+      });
+    });
 });
