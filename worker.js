56f72a8a7ab6fd9f3642ba64963af2d60697bc3f
Jérôme Desboeufs <jerome.desboeufs@gmail.com>
2017-08-13 11:33:39 +0200

    Remove ssl-root-cas and enforce security

--- worker.js
+++ worker.js
@@ -1,11 +1,5 @@
 'use strict'
 
-// To remove in the future
-process.env.NODE_TLS_REJECT_UNAUTHORIZED = '0'
-
-// More cas
-require('ssl-root-cas/latest').inject()
-
 // Configure
 require('./lib/config/mongoose')
 require('./lib/config/jobs')
