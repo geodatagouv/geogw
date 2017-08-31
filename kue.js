7d8341fae42d264c958e2f65edca914dadbe586d
Jérôme Desboeufs <jerome.desboeufs@gmail.com>
2017-08-31 23:47:39 +0200

    Use delayed-jobs

--- kue.js
+++ kue.js
@@ -1,12 +1,10 @@
 'use strict'
-
 /* eslint no-console: off */
-const { configure, getApp } = require('./lib/util/jobs')
+
+require('./lib/config/jobs')
+
+const { getApp } = require('delayed-jobs')
 
 const port = process.env.PORT || 3030
-const jobsConfig = require('./lib/config/jobs')
-
-configure(jobsConfig)
 getApp().listen(port)
-
 console.log(`Kue App is listening on port ${port}`)
--- worker.js
+++ worker.js
@@ -1,4 +1,4 @@
 'use strict'
 
 require('./lib/init')
-require('./lib/util/jobs').startProcessing()
+require('delayed-jobs').startProcessing()
